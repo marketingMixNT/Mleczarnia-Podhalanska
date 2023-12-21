0e1e7aa14501942da5f4b3efa705523d6336927c
Marek <marekgacek45@gmail.com>
2023-12-21 11:07:10 +0100

    add gallery and change in all product page

--- src/js/main.js
+++ src/js/main.js
@@ -98,25 +98,76 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //SHUFFLE ALL PRODUCTS
 
-document.addEventListener("DOMContentLoaded", () => {
-    const container = document.getElementById('products__container');
-    const divs = Array.from(container.children); 
-    const shuffledDivs = divs.sort(() => 0.5 - Math.random()); 
-
-    container.innerHTML = '';
-
-    shuffledDivs.forEach(div => container.appendChild(div));
-});
-
-// const shuffleProducts = (containerId) => {
-//     const container = document.getElementById(containerId);
-//     const divs = Array.from(container.children);
-//     const shuffledDivs = divs.sort(() => 0.5 - Math.random());
+// document.addEventListener("DOMContentLoaded", () => {
+//     const container = document.getElementById('products__container');
+//     const divs = Array.from(container.children); 
+//     const shuffledDivs = divs.sort(() => 0.5 - Math.random()); 
 
 //     container.innerHTML = '';
+
 //     shuffledDivs.forEach(div => container.appendChild(div));
-// }
-// document.addEventListener("DOMContentLoaded", () => {
-//     shuffleProducts('other-products__container');
-//     shuffleProducts('products__container');
 // });
+
+
+
+
+//GALLERY 
+const filterItem = document.querySelector('.items-links');
+const filterImages = document.querySelectorAll('.project-img');
+const filterInfo = document.querySelector('.info');
+
+if (filterItem && filterImages.length > 0) {
+    window.addEventListener('load', () => {
+        const initialFilter = 'sklep';
+
+        filterImages.forEach((image) => {
+            let filterImageName = image.getAttribute('data-name');
+            if (filterImageName === initialFilter) {
+                image.style.display = "block";
+            } else {
+                image.style.display = "none";
+            }
+        });
+
+        if (filterInfo) {
+            filterInfo.querySelectorAll('[data-name]').forEach((infoItem) => {
+                let infoName = infoItem.getAttribute('data-name');
+                if (infoName === initialFilter) {
+                    infoItem.style.display = "block";
+                } else {
+                    infoItem.style.display = "none";
+                }
+            });
+        }
+
+        filterItem.querySelector(`[data-name="${initialFilter}"]`).classList.add('menu-active');
+
+        filterItem.addEventListener('click', (selectedItem) => {
+            if (selectedItem.target.classList.contains('item-link')) {
+                document.querySelector('.menu-active').classList.remove('menu-active');
+                selectedItem.target.classList.add('menu-active');
+                let filterName = selectedItem.target.getAttribute('data-name');
+
+                filterImages.forEach((image) => {
+                    let filterImageName = image.getAttribute('data-name');
+                    if (filterImageName === filterName || filterName === 'all') {
+                        image.style.display = "block";
+                    } else {
+                        image.style.display = "none";
+                    }
+                });
+
+                if (filterInfo) {
+                    filterInfo.querySelectorAll('[data-name]').forEach((infoItem) => {
+                        let infoName = infoItem.getAttribute('data-name');
+                        if (infoName === filterName || filterName === 'all') {
+                            infoItem.style.display = "block";
+                        } else {
+                            infoItem.style.display = "none";
+                        }
+                    });
+                }
+            }
+        });
+    });
+}
